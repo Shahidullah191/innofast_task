0d183048191b54ba43c1bff6b9694d7810d34b2b
Shahidullah191 <shahidullah.cse.gub@gmail.com>
2025-05-16 22:00:28 +0600

    add logo

--- lib/utils/images.dart
+++ lib/utils/images.dart
@@ -1,10 +1,11 @@
 class Images {
   const Images._();
 
-  static String get logo => 'logo'.jpg;
+  static String get logo => 'logo'.png;
   static String get placeholder => 'placeholder'.jpg;
 }
 
 extension on String {
   String get jpg => 'assets/image/$this.jpg';
+  String get png => 'assets/image/$this.png';
 }
